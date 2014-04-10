1350f3f1a07beaeaa1f7f84deb836ffa8fcd21bc
asuran1zala <wkchoi@ualberta.ca>
2014-04-10 13:24:08 -0600

    Did some testing

--- TuneInTest/src/cs/ualberta/ca/tuneintest/MainActivityTest.java
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/MainActivityTest.java
@@ -1,6 +1,10 @@
 package cs.ualberta.ca.tuneintest;
 
+import java.util.WeakHashMap;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
@@ -8,12 +12,15 @@ import android.test.TouchUtils;
 import android.view.KeyEvent;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import cs.ualberta.ca.tunein.MainActivity;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
-	Activity activity;
+	MainActivity activity;
 	EditText textInput;
+	EditText textLong;
+	EditText textLat;
 	Instrumentation instrumentation;
 	
 	public MainActivityTest() {
@@ -26,6 +33,8 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		activity = getActivity();
 
 		textInput = ((EditText) activity.findViewById(cs.ualberta.ca.tunein.R.id.edit_username));
+		textLong = ((EditText) activity.findViewById(cs.ualberta.ca.tunein.R.id.textViewInputChangeLong));
+		textLat = ((EditText) activity.findViewById(cs.ualberta.ca.tunein.R.id.textViewInputChangeLat));
 	}
 	
 	
@@ -55,11 +64,11 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		this.sendKeys(KeyEvent.KEYCODE_BACK);
 	}
 	
-	public void testchangename()throws Throwable{
-		runTestOnUiThread(new Runnable(){
+	public void testchangename()throws Throwable {
+		runTestOnUiThread(new Runnable() {
 			
 			@Override
-			public void run(){
+			public void run() {
 			
 				// TODO Auto-generated method stub
 				setname("Changed Name");
@@ -68,11 +77,34 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		
 	}
 	
-	public void setname(String name){
+	public void setname(String name) {
 		assertNotNull(activity.findViewById(cs.ualberta.ca.tunein.R.id.name_button));
 		textInput.setText(name);
 		((Button) activity.findViewById(cs.ualberta.ca.tunein.R.id.name_button)).performClick();	
 	}
 	
+	public void testViewProfile() {
+	    ActivityMonitor monitor = getInstrumentation().addMonitor(cs.ualberta.ca.tunein.ProfileViewActivity.class.getName(), null, false);
+		
+		Button button = (Button) activity.findViewById(cs.ualberta.ca.tunein.R.id.buttonViewProfile);
+		TouchUtils.clickView(this, button);
+		
+		cs.ualberta.ca.tunein.ProfileViewActivity secondActivity = (cs.ualberta.ca.tunein.ProfileViewActivity) monitor
+		          .waitForActivityWithTimeout(5);
+		assertNotNull(secondActivity);
+		this.sendKeys(KeyEvent.KEYCODE_BACK);
+	}
+	
+	public void testLoadGPS() {
+		ImageButton button = (ImageButton) activity.findViewById(cs.ualberta.ca.tunein.R.id.imageButtonRefreshLoc);
+		assertNotNull("Button should not be null", button);
+		TouchUtils.clickView(this, button);
+	}
+	
+	public void testSetLocation() {
+		Button button = (Button) activity.findViewById(cs.ualberta.ca.tunein.R.id.otherLocation_button);
+		assertNotNull("Button should not be null", button);
+		TouchUtils.clickView(this, button);
+	}
 
 }
--- TuneInTest/src/cs/ualberta/ca/tuneintest/commentViewAdapterTest.java
+++ TuneInTest/src/cs/ualberta/ca/tuneintest/commentViewAdapterTest.java
@@ -11,13 +11,14 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.test.TouchUtils;
 import android.view.KeyEvent;
 import android.widget.Button;
+import cs.ualberta.ca.tunein.CommentPageActivity;
 import cs.ualberta.ca.tunein.CommentViewAdapter;
 import cs.ualberta.ca.tunein.TopicListActivity;
 
 @SuppressLint("NewApi")
 public class commentViewAdapterTest extends ActivityInstrumentationTestCase2<TopicListActivity> {
 	
-	Activity activity;
+	TopicListActivity activity;
 	Instrumentation instrumentation;
 	
 	public commentViewAdapterTest() {
@@ -32,12 +33,11 @@ public class commentViewAdapterTest extends ActivityInstrumentationTestCase2<Top
 	}
 	
 	public void testViewingCommentButton() {
-		activity = getActivity();
 		Button button = (Button) activity.findViewById(cs.ualberta.ca.tunein.R.id.buttonView);
 		assertNotNull("Button should not be null", button);
 		
-		ActivityMonitor monitor = getInstrumentation().addMonitor(cs.ualberta.ca.tunein.CommentPageActivity.class.getName(), null, false);
 		
+		ActivityMonitor monitor = getInstrumentation().addMonitor(cs.ualberta.ca.tunein.CommentPageActivity.class.getName(), null, false);
 		TouchUtils.clickView(this, button);
 		
 		cs.ualberta.ca.tunein.CommentPageActivity secondActivity = (cs.ualberta.ca.tunein.CommentPageActivity) monitor
@@ -46,9 +46,9 @@ public class commentViewAdapterTest extends ActivityInstrumentationTestCase2<Top
 		this.sendKeys(KeyEvent.KEYCODE_BACK);
 	}
 	
-	public void testReplyButton(){
-		activity = getActivity();
+	public void testReplyButton() {
 		Button button = (Button) activity.findViewById(cs.ualberta.ca.tunein.R.id.buttonReply);
+		assertNotNull("Button should not be null", button);
 		TouchUtils.clickView(this, button);
 		this.sendKeys(KeyEvent.KEYCODE_BACK);
 		
